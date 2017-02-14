9960078f933b43259aac5d37bb548ef6ebe1375d
Jing Peng <pj.hades@gmail.com>
2017-02-13 22:15:28 -0500

    Add unit tests
    - pipes
    - escape sequences

---
+++ test/test_shell.rb
@@ -0,0 +1,54 @@
+require 'minitest/autorun'
+
+require_relative '../script.rb'
+
+class TestParsing < MiniTest::Unit::TestCase
+    def test_pipe
+        cases = [
+            ['', []],
+            ['foo -a -b -c -d', [['foo', '-a', '-b', '-c', '-d']]],
+            ['foo | bar | baz', [['foo'], ['bar'], ['baz']]],
+            ['foo|bar|baz', [['foo'], ['bar'], ['baz']]],
+            ['foo -f1 -f2 | bar -b1', [['foo', '-f1', '-f2'],
+                                       ['bar', '-b1']]],
+            # unfinished pipe
+            ['foo|',      [false, 4]],
+            # no command between pipes
+            ['foo||bar',  [false, 4]],
+            ['foo| |bar', [false, 5]],
+        ]
+
+        cases.each do |arg, expected|
+            assert_equal expected, parse_pipe_and_quote(arg)
+        end
+    end
+
+    def test_escape_sequence
+        cases = [
+            # \n\r\t
+            ['\n\r\t', [["\n\r\t"]]],
+            # ' |\n\r\t'
+            ['\' |\n\r\t\'', [[' |\n\r\t']]],
+            # " |\n\r\t"
+            ['" |\n\r\t"', [[" |\n\r\t"]]],
+            # '"'
+            ['\'"\'', [['"']]],
+            # "'"
+            ['"\'"', [['\'']]],
+            # '\"'
+            ['\'\\"\'', [['\\"']]],
+            # '\\'
+            ['\'\\\\\'', [['\\\\']]],
+            # "\\"
+            ['"\\\\"', [['\\']]],
+
+            # unfinished quotes
+            ['foo \'bar', [false, 8]],
+            ['foo "bar', [false, 8]],
+        ]
+
+        cases.each do |arg, expected|
+            assert_equal expected, parse_pipe_and_quote(arg)
+        end
+    end
+end
