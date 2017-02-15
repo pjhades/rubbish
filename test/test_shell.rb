176033a7083b7c6b8480827cfb366a6e5cf1250b
Jing Peng <pj.hades@gmail.com>
2017-02-14 21:51:25 -0500

    Add unit test for redirection parsing

--- test/test_shell.rb
+++ test/test_shell.rb
@@ -51,4 +51,40 @@ class TestParsing < MiniTest::Unit::TestCase
             assert_equal expected, parse_pipe_and_quote(arg)
         end
     end
+
+    def test_redir
+        cases = [
+            [['foo', '>x', '-b', '<y', '-a'],
+             [['foo', '-b', '-a'],
+              {$stdin  => [['y', 'r']],
+               $stdout => [['x', 'w']],
+               $stderr => [],
+              }]],
+
+            [['foo', '<x1', '<x2', '>y1', '>y2'],
+             [['foo'],
+              {$stdin  => [['x1', 'r'], ['x2', 'r']],
+               $stdout => [['y1', 'w'], ['y2', 'w']],
+               $stderr => [],
+              }]],
+
+            [['foo', '<x', '>y1', '&>y2'],
+             [['foo'],
+              {$stdin  => [['x', 'r']],
+               $stdout => [['y1', 'w'], ['y2', 'w']],
+               $stderr => [['y2', 'w']],
+              }]],
+
+            [['foo', '<x', '&>y'],
+             [['foo'],
+              {$stdin  => [['x', 'r']],
+               $stdout => [['y', 'w']],
+               $stderr => [['y', 'w']],
+              }]],
+        ]
+
+        cases.each do |arg, expected|
+            assert_equal expected, parse_redir(arg)
+        end
+    end
 end
